deac12d6a3b795b1665a0af9a6137b88fb5e262a
RKrasnov <r.krsanov@systtech.ru>
2019-09-11 16:08:07 +0200

    Добавки для material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
